87788a75290d65373e6a1b5a8699f85215890508
Ayoub Louah <ayoub.louah10@gmail.com>
2026-02-21 16:19:20 +0100

    DJ Posaxa V2.0

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -24,7 +24,7 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/Fotos/logo.png'
+    icon: '/Fotos/dj-posaxa-logo.png'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
